3e9ab90ae3dc18b0d19e7e9fb4a856638644c73b
Ponkratov Nickolay <45602069+Vedji@users.noreply.github.com>
2024-03-05 17:42:43 +0300

    Add files via upload

--- bdd_test.feature
+++ bdd_test.feature
@@ -1,21 +1,20 @@
 Feature: Field of Miracles
-  Test Field of miracles to avoid mistakes
   Scenario: Generate word
     Given Generate word by seed 2
     When Print guess word
     Then Get printed guess word
 
-  Scenario: Guess letter
+  Scenario: Guess letter test
     Given Guess letter ч
     When Check input letter
-    Then print letter guess
+    Then Print letter guess
 
   Scenario: Not guess letter
-    Given Guess letter п
-    When Check input letter
-    Then print letter not guess
+    Given Not Guess letter п
+    When Not Check input letter
+    Then Print letter not guess
 
   Scenario: Not generated
-    Given Guess letter а not generated
+    Given Not generated Guess letter а
     When Check input letter not generated
     Then Print error
--- steps/test_field_of_miracles.py
+++ steps/test_field_of_miracles.py
@@ -4,22 +4,22 @@ import field_of_miracles
 
 
 @given('Generate word by seed {number}')
-def step_generate(context, number):
+def step_generate_given(context, number):
     random.seed(number)
 
 
 @when('Print guess word')
-def step_generate(context):
+def step_generate_when(context):
     context.result = field_of_miracles.FieldOfMiracles().generate_word()
 
 
 @then('Get printed guess word')
-def step_generate(context):
+def step_generate_then(context):
     assert context.result == 'Слово длинной 3 сгенерированно'
 
 
 @given('Guess letter {letter}')
-def step_guess(context, letter):
+def step_guess_given(context, letter):
     context.letter = letter
     random.seed(2)
     context.miracles = field_of_miracles.FieldOfMiracles()
@@ -27,37 +27,45 @@ def step_guess(context, letter):
 
 
 @when('Check input letter')
-def step_guess(context):
+def step_guess_when(context):
     context.result = context.miracles.guess_letter(context.letter)
 
 
-@then('print letter guess')
-def step_guess(context):
+@then('Print letter guess')
+def step_guess_then(context):
     assert context.result == 'Буква ч угадана, ваше слово ч__'
 
 
-@then('print letter not guess')
-def step_guess(context):
+@given('Not Guess letter {letter}')
+def step_not_guess_given(context, letter):
+    context.letter = letter
+    random.seed(2)
+    context.miracles = field_of_miracles.FieldOfMiracles()
+    context.miracles.generate_word()
+
+
+@when('Not Check input letter')
+def step_not_guess_when(context):
+    context.result = context.miracles.guess_letter(context.letter)
+
+
+@then('Print letter not guess')
+def step_not_guess_then(context):
     assert context.result == f'В слове ___ нет буквы п'
 
 
-@given('Guess letter {letter} not generated')
-def step_not_generate(context, letter):
+@given('Not generated Guess letter {letter}')
+def step_not_generate_given(context, letter):
     context.letter = letter
     random.seed(2)
     context.miracles = field_of_miracles.FieldOfMiracles()
 
 
-@when('Check input letter')
-def step_guess(context):
-    context.result = context.miracles.guess_letter(context.letter)
-
-
 @when('Check input letter not generated')
-def step_guess(context):
+def step_not_generate_when(context):
     context.result = context.miracles.guess_letter(context.letter)
 
 
 @then('Print error')
-def step_guess(context):
+def step_not_generate_then(context):
     assert context.result == 'Сначала загадайте слово'
